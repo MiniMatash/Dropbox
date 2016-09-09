f4c3eb0c06b4d031b0481c221f13324652a8b4ba
d.matashev <d,matashev@aimprosoft.com>
2016-09-09 18:15:50 +0300

    appearens rework + links

--- src/main/webapp/resources/js/getFileTree.js
+++ src/main/webapp/resources/js/getFileTree.js
@@ -2,19 +2,23 @@ function getFileTree() {
     $.ajax({
         url: "/getFileTree",
         method: "GET",
-        data:{path:window.location.pathname},
+        data: {path: window.location.pathname},
         success: function (files) {
             var table = $("<table id='tableBody'>").appendTo($("#filesBody"));
             $(table).append($('<tr><th width="10%"></th><th width="20%">Name</th><th width="15%">Changed date</th><th width="10%">Authorization level</th></tr>'));
-            if(window.location.pathname!="/home")
+            if (window.location.pathname != "/home")
                 $("<tr>").appendTo(table)
-                    .append('<span><a href="'+window.location.href.substring(0,window.location.href.lastIndexOf("/"))+'">...</a></span>');
+                    .append('<span><a href="' + window.location.href.substring(0, window.location.href.lastIndexOf("/")) + '">...</a></span>');
             for (var file in files) {
-                $("<tr>").appendTo(table)
-                    .append('<td><img src="/images/files/'+files[file].type+'.png">')
-                    .append('<td style="text-align: center">' + files[file].name + '')
-                    .append('<td>' + files[file].modificationDate + '')
-                    .append('<td>' + "--" + '');
+                var tr = $("<tr>").appendTo(table)
+                $(tr).append('<td><img src="/images/files/' + files[file].type + '.png">');
+                if (files[file].type == "folder") {
+                    $(tr).append($('<td style="text-align: center"><a href="' + window.location.pathname + "/" + files[file].name + '">' + files[file].name + '</a>'));
+                } else {
+                    $(tr).append($('<td style="text-align: center">' + files[file].name + ''));
+                }
+                $(tr).append($('<td>' + files[file].modificationDate + ''));
+                $(tr).append($('<td>' + "--" + ''));
             }
         },
         error: function (xhr, ajaxOptions, thrownError) {
